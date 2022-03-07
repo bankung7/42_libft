ab4bb505a2f9848bc4559450349d660f363fa287
Vorravoot Nilprapan <vnilprap@z2t9c2.42bangkok.com>
2022-03-07 11:16:48 +0700

    master

--- strtrim.c
+++ strtrim.c
@@ -6,36 +6,21 @@
 /*   By: vnilprap <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 08:07:25 by vnilprap          #+#    #+#             */
-/*   Updated: 2022/03/04 10:06:34 by vnilprap         ###   ########.fr       */
+/*   Updated: 2022/03/07 11:13:01 by vnilprap         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		len;
 	char	*ptr;
 
-	i = 0;
-	len = 0;
-	while (s1[i])
-	{
-		if (ft_strchr(set, (char)s1[i]) != 0)
-			len++;
-		i++;
-	}
-	ptr = malloc(sizeof(char) * (ft_strlen((char *)s1) - len + 1));
-	if (!ptr)
+	if (!s1)
 		return (0);
-	i = 0;
-	len = 0;
-	while (s1[i])
+	if (!set)
 	{
-		if (ft_strchr(set, (char)s1[i]) == 0)
-			ptr[len++] = (char)s1[i];
-		i++;
+		ptr = malloc(sizeof(char) * ft_strlen((char *)s1) + 1);
+		ft_strlcpy(ptr, s1, ft_strlen((char *)s1));
+		return (ptr);
 	}
-	ptr[len] = 0;
-	return (ptr);
 }
